b063c24ef1251126846826503a424dbd354df984
Yannik Schroeder <yannik131@web.de>
2020-10-09 16:31:31 +0200

    scripts

--- scripts_macos/load_database.sh
+++ scripts_macos/load_database.sh
@@ -2,6 +2,6 @@
 dir=${PWD}/../postgres_csv
 psql -c "drop database activities;"
 psql -c "create database activities;"
-./manage.py makemigrations
-./manage.py migrate
+python3 $dir/../manage.py makemigrations
+python3 $dir/../manage.py migrate
 psql -d activities -c "copy activity_category from '$dir/categories.csv' with (format csv); copy activity_activity from '$dir/activities.csv' with (format csv); copy account_location from '$dir/locations.csv' with (format csv); copy account_user from '$dir/users.csv' with (format csv); copy account_user_user_permissions from '$dir/user_permissions.csv' with (format csv);"
