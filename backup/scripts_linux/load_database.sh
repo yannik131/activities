2ef37b0c9d0547de7c3ca2453bbcf1cbd9152c86
Yannik Schroeder <yannik131@web.de>
2020-10-09 16:45:12 +0200

    scripts

--- backup/scripts_linux/load_database.sh
+++ backup/scripts_linux/load_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dir=${PWD}/../postgres_csv
+dir=${PWD}/../postgres_csv_linux
 sudo -u postgres psql -c "drop database activities;"
 sudo -u postgres psql -c "create database activities;"
-python3 $dir/../manage.py makemigrations account activity chat competitions scheduling usergroups vacancies wall
-python3 $dir/../manage.py migrate
+python3 $dir/../../manage.py makemigrations account activity chat competitions scheduling usergroups vacancies wall
+python3 $dir/../../manage.py migrate
 sudo -u postgres psql -d activities -c "copy activity_category from '$dir/categories.csv' with (format csv); copy activity_activity from '$dir/activities.csv' with (format csv); copy account_location from '$dir/locations.csv' with (format csv); copy account_user from '$dir/users.csv' with (format csv); copy account_user_user_permissions from '$dir/user_permissions.csv' with (format csv);"
