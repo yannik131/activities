f69bd22fa214219d2c6fe1b1b34f268fda8b123c
Yannik Schroeder <yannik131@web.de>
2021-01-30 23:11:53 +0100

    30.01.2021

--- scripts/sqlsequencereset.sh
+++ scripts/sqlsequencereset.sh
@@ -2,5 +2,5 @@ source scripts/comp.sh
 for app in "account" "usergroups" "vacancies" "wall" "competitions"
 do
     python3 manage.py sqlsequencereset $app > scripts/sqlreset.sql
-    sudo psql $option -d activities -f scripts/sqlreset.sql
+    sudo $option psql -d activities -f scripts/sqlreset.sql
 done
