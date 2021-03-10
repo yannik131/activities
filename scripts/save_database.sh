1763c2c577eb190d5a35e9d6f4b26d542d4e8168
root <root@h2910280.stratoserver.net>
2021-03-10 16:34:09 +0100

    10.03.21

--- scripts/save_database.sh
+++ scripts/save_database.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-dir=${PWD}/scripts/csv
-source ${PWD}/scripts/comp.sh
-sudo $option psql -d activities -c \
-"copy activity_category_translation to '$dir/category_translation.csv' with (format csv);
-copy activity_activity_translation to '$dir/activity_translation.csv' with (format csv); 
-copy activity_category to '$dir/categories.csv' with (format csv); 
-copy activity_activity to '$dir/activities.csv' with (format csv);"
+sudo -u postgres pg_dump -F c -f backup/backup.sql
