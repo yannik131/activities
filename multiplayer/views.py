c0607fe24870afb40a7c44d7cf5fa3315a7e8f3c
Yannik <yannik131@web.de>
2021-04-06 18:38:15 +0200

    06.04.21

--- multiplayer/views.py
+++ multiplayer/views.py
@@ -13,6 +13,8 @@ from .decorators import guard_match
 
 def lobby(request, activity_name):
     activity = get_object_or_404(Activity, translations__language_code=request.LANGUAGE_CODE, translations__name=activity_name)
+    if activity not in request.user.activities.all():
+        activity.members.add(request.user)
     online_list = activity.members.filter(channel_name__isnull=False)
     user_matches = request.user.multiplayer_matches.filter(activity__id=activity.id)
     return render(request, 'multiplayer/lobby.html', dict(activity=activity, online_list=online_list, user_matches=user_matches))
