9e732415997354df27297b874d71ddf2026270d0
Yannik Schroeder <yannik131@web.de>
2021-05-18 14:55:48 +0200

    18.05.21

--- up.sh
+++ up.sh
@@ -6,5 +6,5 @@ uwsgi --ini config/uwsgi.ini &
 redis --port 6655 &
 daphne -u /tmp/daphne.sock activities.asgi:application &
 nginx
-celery -A activities beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-celery -A activities worker &
+sudo -u not_root celery -A activities beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+sudo -u not_root celery -A activities worker &
