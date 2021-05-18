1ca7b718ac84303729e5e288c0707a96d3088d09
Yannik Schroeder <yannik131@web.de>
2021-05-18 16:10:08 +0200

    18.05.21

--- up.sh
+++ up.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-export DJANGO_SETTINGS_MODULE=activities.settings.pro
 ps -C "uwsgi nginx" -o pid= | xargs kill -9
 uwsgi --ini config/uwsgi.ini &
 redis --port 6655 &
 daphne -u /tmp/daphne.sock activities.asgi:application &
 nginx
-sudo -u not_root celery -A activities beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-sudo -u not_root celery -A activities worker &
+celery -A activities beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A activities worker --uid not_root &
